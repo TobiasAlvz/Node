d9554c9e01bd04e3e57c6617cbc3720ce60a9f20
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-04-28 14:22:00 -0300

    Create app.js

---
+++ buffer/app.js
@@ -0,0 +1,13 @@
+const buffer = Buffer.from ('Olá Mundo');
+
+console.log (buffer);
+console.log (buffer.toString ());
+
+// Convertendo para texto
+console.log(buffer.toString());
+
+// Tamanho em bytes
+console.log(buffer.length);
+
+// Acessando byte específico
+console.log(buffer[0]);
